d74ea62d57d1c962d0600018c15a2cd544b42471
Daniel <robotright@gmail.com>
2011-10-23 23:07:58 +0800

    refactor main method

--- robots_lasers/robots.rb
+++ robots_lasers/robots.rb
@@ -3,34 +3,32 @@ require 'ruby-debug'
 class Robots
   def directions(scenario)
     directions = "GO WEST"
-    north, belt, south = scenario
+    n_lasers, belt, s_lasers = scenario
     clicks = belt.split('').count
     pos = belt.index('X')
 
     n_fire_at, s_fire_at = find_fire_at(belt)
 
-    # north damage
-    n_lasers = north.split('').map{ |n| n == '|' ? 1 : 0 }
-    # bitwise AND to find when robot will take damage
-    n_damage = n_lasers.join.to_i(2) & n_fire_at.join.to_i(2)
-    n_damage = n_damage.to_s(2).split('').map{ |n| n.to_i }
-    # left pad with zeros
-    (clicks - n_damage.count).times { n_damage.insert(0, 0) }
+    n_damage_at = find_damage_at(n_lasers, n_fire_at, clicks)
+    s_damage_at = find_damage_at(s_lasers, s_fire_at, clicks)
 
-    # south damage
-    s_lasers = south.split('').map{ |s| s == '|' ? 1 : 0 }
-    # bitwise AND to find when robot will take damage
-    s_damage = s_lasers.join.to_i(2) & s_fire_at.join.to_i(2)
-    s_damage = s_damage.to_s(2).split('').map{ |s| s.to_i }
-    # left pad with zeros
-    (clicks - s_damage.count).times { s_damage.insert(0, 0) }
-
-    west_damage = n_damage[0..pos].count(1) + s_damage[0..pos].count(1)
-    east_damage = n_damage[pos..clicks].count(1) + s_damage[pos..clicks].count(1)
+    west_damage = n_damage_at[0..pos].count(1) + s_damage_at[0..pos].count(1)
+    east_damage = n_damage_at[pos..clicks].count(1) + s_damage_at[pos..clicks].count(1)
 
     directions = "GO EAST" if west_damage > east_damage
 
     directions
+  end
+
+  def find_damage_at(lasers, fire_at, clicks)
+    lasers = lasers.split('').map{ |l| l == '|' ? 1 : 0 }
+    # bitwise AND to find when lasers will fire
+    damage_at = lasers.join.to_i(2) & fire_at.join.to_i(2)
+    damage_at = damage_at.to_s(2).split('').map{ |n| n.to_i }
+    # left pad with zeros
+    (clicks - damage_at.count).times { damage_at.insert(0, 0) }
+
+    damage_at
   end
 
   def find_fire_at(p)
