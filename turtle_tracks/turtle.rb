5a7345a334d5dc5bf1f988bff0c8931ec3936d15
Daniel <robotright@gmail.com>
2011-10-24 14:26:04 +0800

    drawing north/south works

--- turtle_tracks/turtle.rb
+++ turtle_tracks/turtle.rb
@@ -1,19 +1,52 @@
 require 'ruby-debug'
 
 class Turtle
-  attr_accessor :size, :position, :canvas
+  attr_accessor :size, :x, :y, :canvas, :direction
   MARK = 'X'
 
-  def initialize(size)
+  def initialize(size=9)
     self.size = size
     self.canvas = Array.new(size){ ['.'] * size }
-    mid = self.size / 2
+
     # start turtle in middle of canvas
-    position(mid, mid)
+    mid = self.size / 2
+    self.x = mid
+    self.y = mid
+
+    self.direction = 0
+    mark
   end
 
-  def position(x, y)
-    self.canvas[y][x] = MARK
+  def direction=(value)
+    value -= 360 if value >= 360
+    value += 360 if value < 0
+    @direction = value
+  end
+
+  def execute(command)
+    c, value = command.split
+
+    if c == 'FD'
+      value.to_i.times do
+        if self.direction == 0
+          self.y -= 1
+          mark
+        elsif self.direction == 180
+          self.y += 1
+          mark
+        end
+      end
+    elsif c == 'RT'
+      self.direction += value.to_i
+    end
+  end
+
+  def mark
+    self.canvas[self.y][self.x] = MARK
+  end
+
+  def clear
+    self.canvas.map!{ |y| y.map{ |x| x = '.' } }
   end
 
   def draw
--- turtle_tracks/turtle_spec.rb
+++ turtle_tracks/turtle_spec.rb
@@ -1,10 +1,8 @@
 require './turtle'
 
 describe Turtle do
-  before(:all) do
-    #override stdout
-    #$stdout = String.IO.new
-  end
+  let(:t3) { Turtle.new(3) }
+  let(:t5) { Turtle.new(5) }
 
   describe "#new" do
     it 'create canvas with turtle in middle' do
@@ -13,8 +11,110 @@ describe Turtle do
 . X .
 . . .
 EOS
-      t = Turtle.new(3)
-      t.draw.should == output
+      t3.draw.should == output
+    end
+  end
+
+  describe ".clear" do
+    it 'clears canvas while keeping size' do
+      output =<<-EOS
+. . .
+. . .
+. . .
+EOS
+      t3.clear
+      t3.draw.should == output
+    end
+  end
+
+  # sample command
+  # "RT 135\n"
+  # "LT 135\n"
+  # "FD 5\n"
+
+  describe ".execute(FD 1)" do
+    it 'draw while moving north one click' do
+      output =<<-EOS
+. X .
+. X .
+. . .
+EOS
+      t3.execute("FD 1")
+      t3.draw.should == output
+    end
+  end
+
+  describe ".execute(FD 2)" do
+    it 'draw while moving north two click' do
+      output =<<-EOS
+. . X . .
+. . X . .
+. . X . .
+. . . . .
+. . . . .
+EOS
+      t5.execute("FD 2")
+      t5.draw.should == output
+    end
+  end
+
+  describe ".execute(RT 180); .execute(FD 2)" do
+    it 'draw while moving south two click' do
+      output =<<-EOS
+. . . . .
+. . . . .
+. . X . .
+. . X . .
+. . X . .
+EOS
+      t5.execute("RT 180")
+      t5.execute("FD 2")
+      t5.draw.should == output
+    end
+  end
+
+  describe ".execute(RT 180); .execute(FD 1)" do
+    it 'draw while moving south one click' do
+      output =<<-EOS
+. . .
+. X .
+. X .
+EOS
+      t3.execute("RT 180")
+      t3.execute("FD 1")
+      t3.draw.should == output
+    end
+  end
+
+  describe ".direction=" do
+    it 'can not go above 360' do
+      t3.direction = 270 + 90
+      t3.direction.should == 0
+    end
+
+    it 'can not go below 0' do
+      t3.direction = -90
+      t3.direction.should == 270
+    end
+  end
+end
+
+describe "run turtle with file of instructions" do
+  it 'creates drawing' do
+    file = './samples/simple.logo'
+
+    File.open(file, 'r') do |f|
+      # first 2 lines are for the size
+      size = f.gets.chop.to_i
+      f.gets
+
+      t = Turtle.new(size)
+
+      while instructions = f.gets do
+        #self.canvas << instructions.chop
+      end
+
+      #puts t.draw
     end
   end
 end
