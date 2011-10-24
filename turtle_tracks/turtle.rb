d17108a7ce33e5d55fb3597c7f0636518e8ad63b
Daniel <robotright@gmail.com>
2011-10-24 09:50:01 +0800

    break turtle out into own class

--- turtle_tracks/turtle.rb
+++ turtle_tracks/turtle.rb
@@ -1,36 +1,25 @@
 require 'ruby-debug'
 
 class Turtle
-  attr_accessor :file, :size, :position, :canvas
+  attr_accessor :size, :position, :canvas
   MARK = 'X'
 
-  def initialize(file)
-    self.file = file
-
-    File.open(file, 'r') do |f|
-      # first 2 lines are for the size
-      self.size = f.gets.chop.to_i
-      f.gets
-
-      self.canvas = []
-      self.canvas = Array.new(size){ ['.'] * size }
-
-      mid = self.size / 2
-      position(mid, mid)
-      position(1, 2)
-
-      while instructions = f.gets do
-        #self.canvas << instructions.chop
-      end
-
-      # output canvas
-      self.canvas.each do |a|
-        p a.join(' ')
-      end
-    end
+  def initialize(size)
+    self.size = size
+    self.canvas = Array.new(size){ ['.'] * size }
+    mid = self.size / 2
+    # start turtle in middle of canvas
+    position(mid, mid)
   end
 
   def position(x, y)
     self.canvas[y][x] = MARK
+  end
+
+  def draw
+    # output canvas
+    self.canvas.each do |a|
+      p a.join(' ')
+    end
   end
 end
--- turtle_tracks/turtle_spec.rb
+++ turtle_tracks/turtle_spec.rb
@@ -2,7 +2,37 @@ require './turtle'
 
 describe Turtle do
   describe "#d" do
-    file = './samples/simple.logo'
-    t = Turtle.new(file)
+    it 'blah' do
+      t = Turtle.new(9)
+      t.draw
+    end
   end
 end
+
+#describe "run turtle with file of instructions" do
+  #it 'creates drawing' do
+    #file = './samples/simple.logo'
+
+    #File.open(file, 'r') do |f|
+      ## first 2 lines are for the size
+      #self.size = f.gets.chop.to_i
+      #f.gets
+
+      #self.canvas = []
+      #self.canvas = Array.new(size){ ['.'] * size }
+
+      #mid = self.size / 2
+      #position(mid, mid)
+      #position(1, 2)
+
+      #while instructions = f.gets do
+        ##self.canvas << instructions.chop
+      #end
+
+      ## output canvas
+      #self.canvas.each do |a|
+        #p a.join(' ')
+      #end
+    #end
+  #end
+#end
