9bab62e63243fe1c37158d9e43c38fb8aac3a422
Daniel <robotright@gmail.com>
2011-10-25 09:03:49 +0800

    working version with cleaned up spec

--- robots_lasers/try2/robot.rb
+++ robots_lasers/try2/robot.rb
@@ -3,4 +3,20 @@ require 'ruby-debug'
 scenarios = STDIN.read.split("\n\n")
 scenarios.each do |scenario|
   p scenario.split("\n")
+  north = n.split('')
+  south = s.split('')
+  position = p.index('X')
+  toggle = position.even?
+
+  north.map!.each_with_index{ |v, i| v = (i.even? ^ !toggle) && (v == '|') }
+  south.map!.each_with_index{ |v, i| v = (i.even? ^ toggle) && (v == '|') }
+
+  west = north[0..position].count(true) + south[0..position].count(true)
+  east = north[position..-1].count(true) + south[position..-1].count(true)
+
+  if west > east
+    puts "GO EAST"
+  else
+    puts "GO WEST"
+  end
 end
--- robots_lasers/try2/robot_spec.rb
+++ robots_lasers/try2/robot_spec.rb
@@ -7,24 +7,3 @@ describe "command line attempt" do
     system('cat ./samples/sample-input.txt | ruby robot.rb')
   end
 end
-
-describe "find lasers that will fire on north side" do
-  it "end up with '#|#|#|##'" do
-    n = "#|#|#|##"
-    p = "---X----"
-    s = "###||###"
-
-    north = n.split('')
-    south = s.split('')
-    position = p.index('X')
-    toggle = position.even?
-
-    north.map!.each_with_index{ |v, i| v = (i.even? ^ !toggle) && (v == '|') }
-    south.map!.each_with_index{ |v, i| v = (i.even? ^ toggle) && (v == '|') }
-
-    west = north[0..position].count(true) + south[0..position].count(true)
-    east = north[position..-1].count(true) + south[position..-1].count(true)
-    p west
-    p east
-  end
-end
