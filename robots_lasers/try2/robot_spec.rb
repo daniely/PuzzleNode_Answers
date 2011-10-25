c5bb29e77d8882f0a3ffb290094d3cdcca0ef92f
Daniel <robotright@gmail.com>
2011-10-25 08:37:12 +0800

    comment out slow file reading spec

--- robots_lasers/try2/robot_spec.rb
+++ robots_lasers/try2/robot_spec.rb
@@ -1,5 +1,5 @@
 describe "command line attempt" do
-  it 'runs from the command line' do
-    system('cat ./samples/sample-input.txt | ruby robot.rb > dantest.txt')
-  end
+  #it 'runs from the command line' do
+    #system('cat ./samples/sample-input.txt | ruby robot.rb')
+  #end
 end
