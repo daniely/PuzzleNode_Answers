cae773a711a58690850cd12e127703cbab7b332f
Daniel <robotright@gmail.com>
2011-10-26 21:48:33 +0800

    working version. however, long words take forever
    need to find a more efficient way to do this

--- spelling_suggestions/spelling.rb
+++ spelling_suggestions/spelling.rb
@@ -1,11 +1,35 @@
+require 'ruby-debug'
+
 class Spelling
-  def longest(a, b)
-    a = a.split('')
-    b = b.split('')
+  def closest_match(c, o1, o2)
+    check = c.split('')
+    option1 = o1.split('')
+    option2 = o2.split('')
 
-    a_combo = a.combination(2).to_a.uniq
-    b_combo = b.combination(2).to_a.uniq
+    # loop through all ranges of combos
+    c = check.each_index.map{ |i| check.combination(i).to_a.uniq }[1..-1]
+    option1 = option1.each_index.map{ |i| option1.combination(i).to_a.uniq }[1..-1]
+    option2 = option2.each_index.map{ |i| option2.combination(i).to_a.uniq }[1..-1]
 
-    (a_combo & b_combo).join
+    # find longest substring
+    longest_option1 = 0
+    longest_option2 = 0
+
+    c.each_index do |i|
+      break if c[i].nil? or option1[i].nil?
+      longest_option1 = i unless (c[i] & option1[i]).empty?
+    end
+
+    c.each_index do |i|
+      break if c[i].nil? or option2[i].nil?
+      longest_option2 = i unless (c[i] & option2[i]).empty?
+    end
+
+    # return the longest
+    if longest_option1 > longest_option2
+      o1
+    else
+      o2
+    end
   end
 end
--- spelling_suggestions/spelling_spec.rb
+++ spelling_suggestions/spelling_spec.rb
@@ -2,9 +2,28 @@ require './spelling'
 
 describe "simple case" do
   it 'finds longest substring' do
-    a = "nom"
-    b = "numm"
+    check = "numm"
+    option1 = "nom"
+    option2 = "numb"
+
     s = Spelling.new
-    s.longest(a, b).should == 'nm'
+    s.closest_match(check, option1, option2).should == option2
+  end
+end
+
+describe "from files" do
+  it 'run on sample files' do
+    word_groups = []
+
+    File.open('./samples/SAMPLE_INPUT.txt', 'r') do |f|
+      word_groups = f.read.split("\n\n")
+      word_groups = word_groups[1..-1]
+    end
+
+    s = Spelling.new
+    word_groups.each do |word_group|
+      words = word_group.split("\n")
+      p s.closest_match(words[0], words[1], words[2])
+    end
   end
 end
